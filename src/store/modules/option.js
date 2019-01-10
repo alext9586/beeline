956c8dc6efeae29e6fad2fafe3a56a93b1657f12
Alex T <alext9586@users.noreply.github.com>
2019-01-09 16:56:33 -0800

    Added saving to local storage.

--- src/store/modules/option.js
+++ src/store/modules/option.js
@@ -1,25 +1,42 @@
+import * as localStorageService from "@/services/localStorageService";
+
 export const namespaced = true;
 
 export const state = {
   options: []
 };
 
+const storage = {
+  saveBucket(value) {
+    localStorageService.setItem(localStorageService.storageKeys.bucket, value);
+  }
+};
+
 let nextId = 1;
 export const mutations = {
+  HYDRATE(state, bucket) {
+    state.options = JSON.parse(bucket);
+    nextId = Math.max(bucket.map(b => b.id)) + 1;
+  },
   PUSH(state, option) {
     state.options.push({
       ...option,
       id: nextId++
     });
+    storage.saveBucket(JSON.stringify(state.options));
   },
   DELETE(state, optionToRemove) {
     state.options = state.options.filter(
       option => option.id !== optionToRemove.id
     );
+    storage.saveBucket(JSON.stringify(state.options));
   }
 };
 
 export const actions = {
+  hydrate({ commit, bucket }) {
+    commit("HYDRATE", bucket);
+  },
   add({ commit }, option) {
     commit("PUSH", option);
   },
