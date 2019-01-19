eadd07ded2176cac0dfb5ca40d4d07bdad5b4e8f
Alex T <alext9586@users.noreply.github.com>
2019-01-18 18:30:50 -0800

    Made spinner spin with values

--- src/components/SpinnerContainer.vue
+++ src/components/SpinnerContainer.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="spinner-container">
+    <div class="slot-window">
+      <div class="reel">
+        <h1 v-for="option in spinOptions" :key="option.id">{{option.value}}</h1>
+      </div>
+    </div>
     <div>{{currentValue}}</div>
     <button v-on:click="spin">Spin</button>
   </div>
@@ -9,10 +14,46 @@
 import { mapState, mapActions } from "vuex";
 
 export default {
-  computed: mapState("spin", ["currentValue"]),
+  computed: {
+    spinOptions() {
+      if(this.options.length) {
+        let lastOption = {
+          id: Math.max(...this.options.map(o => o.id)) + 1,
+          value: this.options[0].value
+          };
+        return [...this.options, lastOption];
+      }
+      return [];
+    },
+    ...mapState("spin", ["currentValue", "options"])
+    },
   methods: mapActions("spin", ["spin"])
 };
 </script>
 
 <style scoped>
+.slot-window {
+  position: relative;
+  overflow: hidden;
+  height: 50px;
+  width: 300px;
+  border: 1px solid black;
+}
+
+.reel {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 300px;
+  animation: slideshow 0.5s linear infinite;
+}
+
+.reel > h1 {
+  height: 50px;
+}
+
+@keyframes slideshow {
+  0%    { top: 0%; }
+  100%  { top: -800%; }
+}
 </style>
