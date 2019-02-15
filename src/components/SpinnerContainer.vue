b41dee51afc9ce3b2fcc11bb9697c1170ffbd6a4
Alex T <alext9586@users.noreply.github.com>
2019-02-14 18:01:40 -0800

    Added convincing spin animation

--- src/components/SpinnerContainer.vue
+++ src/components/SpinnerContainer.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="spinner-container">
     <div class="slot-window">
-      <div class="reel">
-        <h1 v-for="option in spinOptions" :key="option.id">{{option.value}}</h1>
+      <div v-if="spinning" class="reel">
+        <h1>{{currentValue}}</h1>
+      </div>
+      <div v-else class="reel-stop">
+        <h1>{{currentValue}}</h1>
       </div>
     </div>
+    <div>{{spinning}}</div>
     <div>{{currentValue}}</div>
     <button v-on:click="spin">Spin</button>
   </div>
@@ -16,17 +20,17 @@ import { mapState, mapActions } from "vuex";
 export default {
   computed: {
     spinOptions() {
-      if(this.options.length) {
+      if (this.options.length) {
         let lastOption = {
           id: Math.max(...this.options.map(o => o.id)) + 1,
           value: this.options[0].value
-          };
+        };
         return [...this.options, lastOption];
       }
       return [];
     },
-    ...mapState("spin", ["currentValue", "options"])
-    },
+    ...mapState("spin", ["currentValue", "options", "spinning"])
+  },
   methods: mapActions("spin", ["spin"])
 };
 </script>
@@ -40,20 +44,47 @@ export default {
   border: 1px solid black;
 }
 
-.reel {
+.reel,
+.reel-stop {
   position: absolute;
   left: 0;
   top: 0;
   width: 300px;
-  animation: slideshow 0.5s linear infinite;
 }
 
-.reel > h1 {
+.reel {
+  animation: slideshow 0.5s linear infinite;
+  filter: blur(2px);
+}
+
+.reel-stop {
+  animation: slideshow-stop 2.5s;
+  animation-timing-function: ease-in-out;
+  animation-iteration-count: 1;
+}
+
+.reel > h1,
+.reel-stop > h1 {
   height: 50px;
+  margin: 0;
+  margin-top: 5px;
 }
 
 @keyframes slideshow {
-  0%    { top: 0%; }
-  100%  { top: -800%; }
+  0% {
+    top: 100%;
+  }
+  100% {
+    top: -100%;
+  }
+}
+
+@keyframes slideshow-stop {
+  0% {
+    top: 100%;
+  }
+  100% {
+    top: 0%;
+  }
 }
 </style>
--- src/store/modules/spin.js
+++ src/store/modules/spin.js
@@ -3,12 +3,14 @@ export const namespaced = true;
 export const state = {
   options: [],
   currentIndex: 0,
-  currentValue: ""
+  currentValue: "",
+  spinning: false
 };
 
 export const mutations = {
   START_SPIN(state, options) {
     state.options = [...options];
+    state.spinning = true;
   },
   ADVANCE_INDEX(state) {
     state.currentIndex =
@@ -16,13 +18,16 @@ export const mutations = {
         ? 0
         : state.currentIndex + 1;
     state.currentValue = state.options[state.currentIndex].value;
+  },
+  STOP_SPIN() {
+    state.spinning = false;
   }
 };
 
 export const actions = {
   spin({ commit, rootState }) {
     commit("START_SPIN", rootState.option.options);
-    const numberOfSpins = Math.floor(Math.random() * 100) + 100;
+    const numberOfSpins = 5; // Math.floor(Math.random() * 100) + 100;
     let spin = 0;
 
     const rotate = () => {
@@ -30,6 +35,8 @@ export const actions = {
         commit("ADVANCE_INDEX");
         if (spin++ < numberOfSpins) {
           rotate();
+        } else {
+          commit("STOP_SPIN");
         }
       }, 500);
     };
